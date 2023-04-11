a6daffc0b66cd0cc05588fd06408e7ebbdd49466
Zach Fuller <zach.fuller1222@gmail.com>
2023-04-10 18:52:13 -0700

    Condensed audio playing into playAudio() function

--- discord_bot_v3/bot.py
+++ discord_bot_v3/bot.py
@@ -2,7 +2,9 @@ import logging
 import discord
 import dotenv
 from discord.commands import Option
-from os import getenv
+from os import getenv, listdir
+from os.path import isfile, join
+from typing import List
 
 
 logging.basicConfig(
@@ -12,6 +14,21 @@ logging.basicConfig(
 )
 dotenv.load_dotenv("config/.env")
 logger = logging.getLogger(__name__)
+
+# Load sound options into list
+soundFileNames: List[str] = []
+soundOpts: List[List[str]] = []
+soundOptsCount = 0
+for file in listdir("resources"):
+    if isfile(join("resources/", file)):
+        if len(soundFileNames) < 25:
+            soundFileNames.append(file[:-4])
+        else:
+            soundOpts.append(soundFileNames.copy())
+            soundFileNames.clear()
+soundOpts.append(soundFileNames)
+logger.info(f"soundOpts: {soundOpts}")
+
 bot = discord.AutoShardedBot(intents=discord.Intents.all())
 logger.info("Starting bot")
 
@@ -24,11 +41,21 @@ async def hello(ctx, name: str = None):
 
 @bot.slash_command()
 async def sound(
-    ctx,
-    opt: Option(
-        str, "Sound to be played", choices=["Crab Rave", "Duck Song"], required=True
-    ),
+    ctx: discord.ApplicationContext,
+    selection: Option(str, "Sound to be played", choices=soundOpts[0], required=True),
 ):
+    await playAudio(ctx, selection)
+
+
+@bot.slash_command()
+async def sound_ext(
+    ctx: discord.ApplicationContext,
+    selection: Option(str, "Sound to be played", choices=soundOpts[1], required=True),
+):
+    await playAudio(ctx, selection)
+
+
+async def playAudio(ctx: discord.ApplicationContext, soundSelection: str):
     vc = ctx.voice_client
     if not vc:
         vc = await ctx.author.voice.channel.connect()
@@ -36,13 +63,28 @@ async def sound(
     if ctx.author.voice.channel.id != vc.channel.id:
         return await ctx.respond("You must be in the same voice channel as the bot")
 
-    # TODO: Select audio source based on opt parameter
+    logger.info(f"{ctx.author.display_name} has chosen to play {soundSelection}")
     vc.play(
         discord.FFmpegPCMAudio(
-            source="C:/Users/Zach/Code/Python/discord-bot-v3/resources/crab_rave.mp3"
+            source=f"C:/Users/Zach/Code/Python/discord-bot-v3/resources/{soundSelection}.mp3"
         )
     )
-    await ctx.message.delete()
+    await ctx.respond(f"{ctx.author.display_name} has instructed iBot to play {soundSelection}!")
+    await vc.disconnect()
+
+
+@bot.slash_command()
+async def shutup(ctx: discord.ApplicationContext):
+    vc = ctx.voice_client
+    if not vc:
+        vc = await ctx.author.voice.channel.connect()
+
+    if ctx.author.voice.channel.id != vc.channel.id:
+        return await ctx.respond("You must be in the same voice channel as the bot")
+
+    logger.info(f"VC is connected? {vc.is_connected()}")
+    await vc.disconnect(force=True)
+    await ctx.respond(f"{ctx.author.display_name} has told iBot to shut up 😔")
 
 
 @bot.event
